5756c63b75f5de158b1e47b1e90d223abfb58ed6
Benjamin Macalister <benjaminmacalister@gmail.com>
2015-04-16 12:04:22 +1000

    updated normal transforms

--- data/Shaders/AnimationVertex.glsl
+++ data/Shaders/AnimationVertex.glsl
@@ -16,7 +16,6 @@ uniform mat4 bones[MAX_BONES];
 void main()
 {
 	frag_texcoord = texcoord;
-	frag_normal = Normal;
 	ivec4 indices = ivec4(boneIndices);
 	vec4 finalPosition = worldPosition;//vec4(30000,16000,-40000, 0);
 	finalPosition += bones[indices.x] * position * boneWeights.x*5;
@@ -24,5 +23,13 @@ void main()
 	finalPosition += bones[indices.z] * position * boneWeights.z*5;
 	finalPosition += bones[indices.w] * position * boneWeights.w*5;
 	finalPosition.w = 1;
+	vec4 finalNormal = vec4(0);
+	finalNormal += bones[indices.x] * Normal * boneWeights.x*5;
+	finalNormal += (bones[indices.y] * Normal * boneWeights.y*5);
+	finalNormal += bones[indices.z] * Normal * boneWeights.z*5;
+	finalNormal += bones[indices.w] * Normal * boneWeights.w*5;
+	finalNormal.w = 1;
+	
+	frag_normal = finalNormal;
 	gl_Position = projection_view * world * finalPosition;
 }
